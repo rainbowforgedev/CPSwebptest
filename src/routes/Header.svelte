294c95a0eb60f2e28eb190875aa46c74c0746162
Rainbow Forge <rainbowforge@proton.me>
2025-03-10 17:57:46 -0400

    Removed other pages and header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -14,7 +14,7 @@
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
-		<ul>
+		<!-- <ul>
 			<li aria-current={page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
@@ -24,7 +24,7 @@
 			<li aria-current={page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
 			</li>
-		</ul>
+		</ul> -->
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg>
