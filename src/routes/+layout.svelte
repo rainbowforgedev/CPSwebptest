d4c6064a1f8e43e93819416f61a91aa58d46ac56
Rainbow Forge <rainbowforge@proton.me>
2025-03-10 18:08:58 -0400

    trying direct img src="" insert without ?url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,17 @@
 <script>
-	import Header from './Header.svelte';
-	import '../app.css';
+		import '../app.css';
 
 	let { children } = $props();
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
 		{@render children()}
 	</main>
 
 	<footer>
 		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
+			<a href="https://rainbowforge.dev/linktree">Blame</a>
 		</p>
 	</footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import Counter from './Counter.svelte';
-	import bhagat from '$lib/images/29132-8-bhagat-singh-photos.webp?url';
+	
+	// import bhagat from '$lib/images/29132-8-bhagat-singh-photos.webp?url';
 	
 </script>
 
@@ -13,9 +13,9 @@
 	<h1>ImageTest
 		</h1>
 		<h3>Direct Src:</h3>
-	<img src="./src/lib/images/29132-8-bhagat-singh-photos.webp?url" alt="Bhagat" type="image/webp" /> 
+	<img src="./src/lib/images/29132-8-bhagat-singh-photos.webp" alt="Bhagat" type="image/webp" /> 
 	<h3>Imported Src:</h3>
-<img src={bhagat} alt="Bhagat" type="image/webp" /> 
+<!-- <img src={bhagat} alt="Bhagat" type="image/webp" />  -->
 	</section>
 
 <style>
